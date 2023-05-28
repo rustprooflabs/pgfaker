b3401e9d301343a69896ac73fac58cc9b90aefae
Ryan Lambert <ryan@rustprooflabs.com>
2023-05-28 08:50:33 -0600

    Add build process for Debian 11 to allow use in geofaker project

---
+++ build/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+BUILDDIR=`pwd`
+BASE=$(dirname `pwd`)
+VERSION=$(cat $BASE/pgfaker.control | grep default_version | cut -f2 -d\')
+LOGDIR=${BASE}/target/logs
+ARTIFACTDIR=${BASE}/target/artifacts
+PGRXVERSION=0.9.0
+
+#PG_VERS=("pg11" "pg12" "pg13" "pg14" "pg15")
+PG_VERS=("pg15")
+
+echo $BASE
+echo $VERSION
+echo $LOGDIR
+echo $ARTIFACTDIR
+echo "pgrx Version: ${PGRXVERSION}"
+
+mkdir -p ${LOGDIR}
+mkdir -p ${ARTIFACTDIR}
+
+
+for image in `ls docker/  ` ; do
+
+    OS_DIST=$(echo ${image}|cut -f2 -d-)
+    OS_VER=$(echo ${image}|cut -f3 -d-)
+
+    echo $OS_DIST
+    echo $OS_VER
+    echo "Pg Version: ${PG_VER}"
+
+    cd ${BUILDDIR}
+
+    cd docker/${image}
+    echo "  Building Docker image: ${image}"
+    docker build -t ${image} --build-arg PGRXVERSION=${PGRXVERSION}  . 2>&1 > ${LOGDIR}/${image}-build.log || exit 1
+
+    for PG_VER in ${PG_VERS[@]} ; do
+
+        echo "Build pgfaker: ${image}-${PG_VER}"
+        docker run \
+            -e pgver=${PG_VER} \
+            -e image=${image} \
+            -v ${BASE}:/build \
+            --rm \
+            ${image} \
+            /bin/bash -c '/build/build/package.sh ${pgver} ${image}' \
+                > ${LOGDIR}/${image}-${PG_VER}-package.sh.log 2>&1 || echo 'Building this version might have encountered error.'
+
+        echo "${image}-${PG_VER}:  finished"
+    done
+
+
+    echo "Copying artifacts for ${OS_DIST} ${OS_VER}"
+    cd $BASE
+
+    for f in $(find target -name "*.deb") $(find target -name "*.rpm") $(find target -name "*.apk"); do
+        echo "copy: ${f}"
+        cp $f $ARTIFACTDIR/
+    done
+done
+
+#tar -zcvf $BUILDDIR/pgfaker-binaries.tar.gz -C $ARTIFACTDIR .
